4c41d5cb54a7d9005587713f304b18ad3eef8f04
Innocent <55434969+InnocentAnyaele@users.noreply.github.com>
2022-09-08 15:41:02 +0000

    updated env

--- queries.js
+++ queries.js
@@ -7,7 +7,7 @@ const createIncidentTable = "CREATE TABLE incidents ( id SERIAL PRIMARY KEY, cli
 // query to post incident to database
 const postIncident = "INSERT INTO incidents (client_id, incident_desc, city, country, date, weather_report) VALUES ($1, $2, $3, $4, $5, $6)"
 
-
+// dummy insert directly into postgres query tool just for testing purposes
 // INSERT INTO incidents (client_id, incident_desc, city, country, date, weather_report) VALUES (1, 'this is a test incident desc', 'Accra', 'Ghana', '2000-07-22', '[{"data": "this is test data"}]')
 
 
